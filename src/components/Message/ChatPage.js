ca62af69925339377e9f9b1502cb99b24f07ddad
Suman Rayamajhi <sams.rayamajhi@gmail.com>
2022-12-16 20:45:41 +0100

    display messages in chat page

--- src/components/Message/ChatPage.js
+++ src/components/Message/ChatPage.js
@@ -2,6 +2,17 @@ import { useLoaderData } from "react-router-dom";
 
 const ChatPage = () => {
   const data = useLoaderData();
-  console.log(data);
+
+  return (
+    <div>
+      {data?.messages?.map((topic) => (
+        <div key={topic.id}>
+          <h2>{topic.type}</h2>
+          <h3>{topic.text}</h3>
+          <p>{topic.creationTime}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 export default ChatPage;
