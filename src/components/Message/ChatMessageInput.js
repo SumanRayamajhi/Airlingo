629e1fd21e202b9b4f546ffcdcae62f95fb23a33
Suman Rayamajhi <sams.rayamajhi@gmail.com>
2022-12-20 20:07:35 +0100

    complete delete method

--- src/components/Message/ChatMessageInput.js
+++ src/components/Message/ChatMessageInput.js
@@ -1,17 +1,14 @@
 import { useState } from "react";
-import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
-// import { Form, FormControl, InputGroup } from "react-bootstrap";
+import { Form, InputGroup } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { AIRLINGO_ACCESS_TOKEN, API_URL } from "../../constants/contants";
 import "bootstrap/dist/css/bootstrap.css";
-import "./ChatMessageInput.css";
 
 const ChatMessageInput = ({ data, onMessagePost }) => {
   const { topicId } = useParams();
 
   const lastMessage =
-    data.messages[data.messages.length - 1]?.type !== "FromUser";
+    data?.messages[data.messages.length - 1]?.type !== "FromUser";
   console.log(lastMessage);
 
   const [content, setContent] = useState("");
@@ -39,18 +36,23 @@ const ChatMessageInput = ({ data, onMessagePost }) => {
 
   return (
     <>
-      <Form onSubmit={onSubmitMessage}>
-        <InputGroup className="mb-3 mt-5 bg-dark overflow-hidden ">
-          <Form.Control
-            className="bg-dark border-0 px-3 text-light fs-4 bg-transparent"
-            placeholder="Write a message"
-            aria-label="Write a message"
-            aria-describedby="basic-addon2"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
-        </InputGroup>
-      </Form>
+      {!lastMessage ? (
+        ""
+      ) : (
+        <Form onSubmit={onSubmitMessage}>
+          <InputGroup className="mb-3 mt-5 bg-dark overflow-hidden ">
+            <Form.Control
+              className="bg-dark border-0 px-3 text-light fs-4 bg-transparent form-control"
+              placeholder="Write a message"
+              aria-label="Write a message"
+              aria-describedby="basic-addon2"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            />
+            <button>Send</button>
+          </InputGroup>
+        </Form>
+      )}
     </>
   );
 };
--- src/components/Message/ChatPage.js
+++ src/components/Message/ChatPage.js
@@ -2,12 +2,33 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ChatMessageInput from "./ChatMessageInput";
 import { AIRLINGO_ACCESS_TOKEN, API_URL } from "../../constants/contants";
+import { TiDelete } from "react-icons/ti";
 import "./ChatPage.css";
 
 const ChatPage = () => {
   // const data = useLoaderData()
   const [data, setData] = useState({ messages: [] });
   const { topicId } = useParams();
+
+  // var clicks = [];
+  // var time = "";
+
+  // const doubleClick = (e) => {
+  //   e.preventDefault();
+  //   clicks.push(new Date().getTime());
+  //   window.clearTimeout(time);
+  //   time = window.setTimeout(() => {
+  //     if (
+  //       clicks.length > 1 &&
+  //       clicks[clicks.length - 1] - clicks[clicks.length - 2] < 500
+  //     ) {
+  //       <TiDelete />;
+  //       <button style={{ height: "3rem", width: "3rem", background: "red" }}>
+  //         Delete
+  //       </button>;
+  //     }
+  //   }, 500);
+  // };
 
   const getData = async () => {
     if (localStorage.getItem(AIRLINGO_ACCESS_TOKEN)) {
@@ -25,7 +46,22 @@ const ChatPage = () => {
 
   useEffect(() => {
     getData();
-  }, [topicId]);
+  }, []);
+
+  const deleteMessage = async () => {
+    if (localStorage.getItem(AIRLINGO_ACCESS_TOKEN)) {
+      const resp = await fetch(`${API_URL}/api/topics/${topicId}/messages`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem(
+            AIRLINGO_ACCESS_TOKEN
+          )}`,
+        },
+      });
+      getData();
+    }
+  };
 
   return (
     <div>
@@ -38,6 +74,14 @@ const ChatPage = () => {
                 : "onRight all"
             }
           >
+            <TiDelete
+              style={{
+                height: "2rem",
+                width: "2rem",
+                color: "white",
+              }}
+              onClick={() => deleteMessage(topic.id)}
+            />
             <h2>{topic.type}</h2>
             <h3>{topic.text}</h3>
             <p>{topic.creationTime}</p>
